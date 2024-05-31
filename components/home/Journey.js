2a005f1c5cd909c27c5957cfe624f2650dc01109
OluEkele <ekeleolu1@gmail.com>
2024-05-31 15:54:47 +0100

    update

--- components/home/Journey.js
+++ components/home/Journey.js
@@ -20,7 +20,7 @@ const Journey = () => {
           <Image src={Blog1} width={350} height={500} alt=''  className='w-[90%] mx-auto rounded-[20px] h-[500px]'/>
           
           <h2 className='font-medium text-[32px] text-[#0b1433] lg:w-[90%] py-6 pb-2 pl-6 hover:text-[#5e53fa]' >Travelers Tapestry: Weaving Stories of Wanderlust</h2>
-          <p className='w-[90%] text-[18px] text-[#0b1433] mx-auto'>Traveler's Tapestry: Where every journey unfolds as a vibrant thread in the fabric of wanderlust, weaving captivating stories of adventure.</p>
+          <p className='w-[90%] text-[18px] text-[#0b1433] mx-auto'>Traveler&rsquo;s Tapestry: Where every journey unfolds as a vibrant thread in the fabric of wanderlust, weaving captivating stories of adventure.</p>
           
         </div>
         <div className='  lg:w-[48%] w-[90%] mb-10 mx-auto'>
@@ -28,7 +28,7 @@ const Journey = () => {
             <Image src={Blog3} width={200} height={200} alt='' className='rounded-[16px] w-[40%]'/>
             <div>
               <h2 className='font-medium lg:text-[18px] text-[16px] pb-4 pl-6 pt-5 text-[#5e53fa]'>September 25, 2023</h2>
-              <h2 className='font-medium lg:text-[26px] text-[#0b1433] text-[18px] pb-4 pl-6 cursor-pointer hover:text-[#5e53fa]' >Embark on Global Journeys: Travigo's Modern Travelers</h2>
+              <h2 className='font-medium lg:text-[26px] text-[#0b1433] text-[18px] pb-4 pl-6 cursor-pointer hover:text-[#5e53fa]' >Embark on Global Journeys: Travigo&rsquo;s Modern Travelers</h2>
             </div>
           </div>
           <div className='bg-[#fff] rounded-[20px] lg:py-4 lg:px-8 px-4 flex items-center mt-10'>
@@ -52,7 +52,7 @@ const Journey = () => {
         
         <div className='text-white flex-col items-center justify-center  top-[15%]'>
           <h2 className='text-[30px] font-medium pb-2 text-center w-[80%] mx-auto z-10'>Ready To Travel? Get Started</h2>
-          <p className='text-[20px] text-center pb-6 lg:w-[70%] w-[90%] mx-auto z-10'>You have seen the process now. It's simple, fast and seamless to explore the world with VisaWone App. Just download the app to get started.</p>
+          <p className='text-[20px] text-center pb-6 lg:w-[70%] w-[90%] mx-auto z-10'>You have seen the process now. It&rsquo;s simple, fast and seamless to explore the world with VisaWone App. Just download the app to get started.</p>
         </div>
         <div className='lg:flex gap-4 items-center justify-center mx-auto w-[80%]'>
         <a href='/' className='rounded-[16px] py-4 px-6 z-10  lg:top-[50%] top-[60%] lg:left-[53%] left-[40%] my-6 border-[0.1px] border-[#fff] flex items-center gap-2 bg-[#0b081d] hover:bg-[#e44f68] transition-[5s] text-[#fff]'><span><IoLogoGooglePlaystore /></span>Get on Android</a>
